fa211d928f4d12d51f54b0199ea31a1dbf0fc03f
Sylvain Pellier <pellier@gmail.com>
2021-01-14 11:28:38 +0100

    corrections

--- exercices/exerciceTroisiemeJour.php
+++ exercices/exerciceTroisiemeJour.php
@@ -10,6 +10,8 @@
 
 <p><a href="exerciceTroisiemeJour.php?calcul=moyenne&nombreB=3&nombreC=5">en cliquant sur ce lien la page exercice3.php doit afficher dans le cadre de solution la moyenne de 3 et 5</a></p>
 
+<p><a href="exerciceTroisiemeJour.php?calcul=addition&nombreB=3&nombreC=5">en cliquant sur ce lien la page exercice3.php doit afficher dans le cadre de solution la moyenne de 3 et 5</a></p>
+
 <p><a href="exerciceTroisiemeJour.php">en cliquant sur ce lien la page exercice3.php doit afficher dans le cadre de solution le mot "rien"</a></p>
 
 
@@ -33,6 +35,66 @@
     if( isset( $_GET["mot"]) === true && $_GET["mot"] === "algebre" )
     {
         echo "algebre et tartufle";
+    }
+
+    if( isset($_GET["calcul"]))
+    {
+        $total = 0;
+        $nb = 0;
+
+        if( isset($_GET["nombreA"]))
+        {
+            $total += $_GET["nombreA"];
+            $nb++;
+        }
+
+        if( isset($_GET["nombreB"]))
+        {
+            $total += $_GET["nombreB"];
+            $nb++;
+        }
+
+        if( isset($_GET["nombreC"]))
+        {
+            $total += $_GET["nombreC"];
+            $nb++;
+        }
+
+        switch( $_GET["calcul"] )
+        {
+            case "moyenne": echo $total / $nb; break;
+            case "addition": echo $total; break;
+
+        }
+
+        //if( !isset($_GET["calcul"]) &&  !isset($_GET["mot"]) &&  !isset($_GET["valeur"])  )
+        if( count($_GET) === 0)
+        {
+            echo "rien";
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
