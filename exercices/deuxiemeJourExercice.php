89976997b5901941e28cd4ad3b7fd91a7a8791bc
Sylvain Pellier <pellier@gmail.com>
2021-01-14 09:37:56 +0100

    corrections

--- exercices/deuxiemeJourExercice.php
+++ exercices/deuxiemeJourExercice.php
@@ -48,6 +48,26 @@ echo "</ul>";
 ///
 ///
 
+
+function affichageChambreService($chambres)
+{
+    echo "<ul>";
+    foreach ($chambres as $chambre) {
+        echo "<li>" . $chambre["nom"] . "(taille : " . $chambre["taille"] . "m2) dont services ";
+
+        echo "<ul>";
+        foreach ($chambre["service"] as $service) {
+            echo "<li>$service</li>";
+        }
+
+        echo "</ul>";
+        echo "</li>";
+
+
+    }
+    echo "</ul>";
+}
+
 ///
 /// exercice 3 :
 ///  afficher : <p>Il y a 3 chambres dans la base de données :</p>
@@ -91,3 +111,30 @@ echo "</ul>";
 /// exercice 4 :
 /// afficher : <p>Il y a une  chambre dans la base de données avec une baignoire :</p>
 ///  <ul><li>la lodge (taille .....</li> ...</ul>
+///
+
+$chambresAvecBaignoire = [];
+foreach($chambres as $chambre)
+{
+    if(  array_search("baignoire",$chambre["service"]))
+    {
+        $chambresAvecBaignoire[] = $chambre;
+    }
+}
+affichageChambreService($chambresAvecBaignoire);
+
+
+
+echo "<ul>";
+foreach($chambres as $chambre)
+{
+    $disposeBaignoire = false;
+
+    foreach($chambre["service"] as $service)
+    {
+        if($service === "baignoire") $disposeBaignoire = true;
+    }
+
+    if($disposeBaignoire)   echo"<li>".$chambre["nom"]."(taille : ".$chambre["taille"]."m2)</li>";
+}
+echo "</ul>";
