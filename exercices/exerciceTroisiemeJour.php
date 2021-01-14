b2b095cf8e2eccfa8895b1a9264c8133b9c03cd0
Sylvain Pellier <pellier@gmail.com>
2021-01-14 08:57:43 +0100

    corrections

---
+++ exercices/exerciceTroisiemeJour.php
@@ -0,0 +1,30 @@
+<h1>Exercice 3 :</h1>
+
+<p><a href="exercice3.php?valeur=tartufle">en cliquant sur ce lien la page exercice3.php doit afficher dans le cadre de solution le mot : tartfufle</a></p>
+
+<p><a href="exercice3.php?mot=algebre">en cliquant sur ce lien la page exercice3.php doit afficher dans le cadre de solution le mot : algebre et tartufle</a></p>
+
+<p><a href="exercice3.php?calcul=moyenne&nombreA=3&nombreB=4&nombreC=7">en cliquant sur ce lien la page exercice3.php doit afficher dans le cadre de solution la moyenne de 3,4 et 7</a></p>
+
+<p><a href="exercice3.php?calcul=moyenne&nombreA=3&nombreB=8">en cliquant sur ce lien la page exercice3.php doit afficher dans le cadre de solution la moyenne de 3 et 8</a></p>
+
+<p><a href="exercice3.php?calcul=moyenne&nombreB=3&nombreC=3">en cliquant sur ce lien la page exercice3.php doit afficher dans le cadre de solution la moyenne de 3 et 8</a></p>
+
+<p><a href="exercice3.php">en cliquant sur ce lien la page exercice3.php doit afficher dans le cadre de solution le mot "rien"</a></p>
+
+
+<hr />
+
+<h2>Solution : </h2>
+
+<div style="border:1px solid red; width: 200px; height: 200px;">
+    <?php
+
+    //votre code ici ///////////////////// début
+
+
+
+
+    /////////////////////////////////////// fin
+    ?>
+</div>
