db90ebd8f50d8ff2ef01df3c7de95851f22b784e
Sylvain Pellier <pellier@gmail.com>
2021-01-20 14:44:23 +0100

    corrections

--- data/chambres.php
+++ data/chambres.php
@@ -4,8 +4,11 @@ $chambres = [];
 
 $chambre1 = [];
 $chambre1["nom"] = "suite";
+$chambre1["description"] = " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula nibh vel congue elementum. Sed varius augue purus, nec pulvinar lacus ornare et. Nam sodales aliquam nisl, non finibus mi. Etiam ut lacus rutrum est blandit cursus eget tincidunt neque. Nulla vitae accumsan urna, quis consequat orci. Sed risus nulla, maximus sed dolor in, consectetur condimentum velit. Donec tellus ipsum, accumsan quis scelerisque at, molestie eget purus. Integer in semper nibh. Maecenas vitae luctus lacus. Cras et diam quam. ";
 $chambre1["taille"] = 100;
 $chambre1["service"] = ["wifi","bluetooth","douche"];
+$chambre1["tarif"] = 54;
+$chambre1["photos"] = ["https://loremflickr.com/320/240/room","https://loremflickr.com/320/240/room","https://loremflickr.com/320/240/room"];
 
 $chambres[] = $chambre1;
 
@@ -13,6 +16,10 @@ $chambre2 = [];
 $chambre2["nom"] = "lodge";
 $chambre2["taille"] = 200;
 $chambre2["service"] = ["wifi","baignoire","baignoire"];
+$chambre2["description"] = " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula nibh vel congue elementum. Sed varius augue purus, nec pulvinar lacus ornare et. Nam sodales aliquam nisl, non finibus mi. Etiam ut lacus rutrum est blandit cursus eget tincidunt neque. Nulla vitae accumsan urna, quis consequat orci. Sed risus nulla, maximus sed dolor in, consectetur condimentum velit. Donec tellus ipsum, accumsan quis scelerisque at, molestie eget purus. Integer in semper nibh. Maecenas vitae luctus lacus. Cras et diam quam. ";
+$chambre2["tarif"] = 43;
+$chambre2["photos"] = ["https://loremflickr.com/320/240/room","https://loremflickr.com/320/240/room","https://loremflickr.com/320/240/room"];
+
 
 $chambres[] = $chambre2;
 
@@ -20,5 +27,8 @@ $chambre3 = [];
 $chambre3["nom"] = "mini";
 $chambre3["taille"] = 50;
 $chambre3["service"] = ["wifi","douche"];
+$chambre3["description"] = " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula nibh vel congue elementum. Sed varius augue purus, nec pulvinar lacus ornare et. Nam sodales aliquam nisl, non finibus mi. Etiam ut lacus rutrum est blandit cursus eget tincidunt neque. Nulla vitae accumsan urna, quis consequat orci. Sed risus nulla, maximus sed dolor in, consectetur condimentum velit. Donec tellus ipsum, accumsan quis scelerisque at, molestie eget purus. Integer in semper nibh. Maecenas vitae luctus lacus. Cras et diam quam. ";
+$chambre3["tarif"] = 120;
+$chambre3["photos"] = ["https://loremflickr.com/320/240/room","https://loremflickr.com/320/240/room","https://loremflickr.com/320/240/room"];
 
 $chambres[] = $chambre3;
