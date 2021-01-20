cf50ab3ec11cb869e6ca32bcdaab779e081537f7
Sylvain Pellier <pellier@gmail.com>
2021-01-20 12:17:30 +0100

    corrections

--- exercices/exerciceQuatriemeJour.php
+++ exercices/exerciceQuatriemeJour.php
@@ -1,16 +1,17 @@
-<form action="" method="get">
-    <input type="text" name="nombreA" />
-    <input type="text" name="nombreB" />
-    <select name="calcul" >
-        <option>addition</option>
-        <option>soustraction</option>
-        <option>division</option>
-        <option>multiplication</option>
-    </select>
-<input type="submit" value="calculer" />
-</form>
-
 <?php
+session_start();
+
+if(!isset($_SESSION["historique"])) $_SESSION["historique"] = [];
+$historique = $_SESSION["historique"];
+
+
+if(isset($_GET["nombreA"])) { $a = $_GET["nombreA"];}
+else if(isset($_COOKIE["nombreA"])) { $a = $_COOKIE["nombreA"];}
+else $a = 0;
+
+if(isset($_GET["nombreB"])) { $b = $_GET["nombreB"];}
+else if(isset($_COOKIE["nombreB"])) { $b = $_COOKIE["nombreB"];}
+else $b = 0;
 
 function addition($nombreA,$nombre)
 {
@@ -20,6 +21,7 @@ function soustraction($nombreA,$nombre)
 {
     return $nombreA - $nombre;
 }
+
 
 function division($nombreA,$nombre)
 {
@@ -35,30 +37,67 @@ function division($nombreA,$nombre)
 
 function multiplication($nombreA,$nombre)
 {
-        return $nombreA * $nombre;
+    return $nombreA * $nombre;
 
 }
 
 
 if(isset($_GET["calcul"]) && !empty($_GET["nombreA"]) && !empty($_GET["nombreB"])) {
 
-    echo"Le résultat du calcul ( {$_GET["calcul"]} ) est =  ";
+    setcookie("nombreA",$_GET["nombreA"],time() + 3600);
+    setcookie("nombreB",$_GET["nombreB"],time() + 3600);
+
 
     switch ($_GET["calcul"]) {
         case "addition":
-            echo addition($_GET["nombreA"],$_GET["nombreB"]);
+            $resultat =  addition($_GET["nombreA"],$_GET["nombreB"]);
+            $operateur = "+";
             break;
         case "soustraction":
-            echo soustraction($_GET["nombreA"],$_GET["nombreB"]);
+            $resultat =  soustraction($_GET["nombreA"],$_GET["nombreB"]);
+            $operateur = "-";
             break;
         case "division":
-            echo division($_GET["nombreA"],$_GET["nombreB"]);
+            $resultat = division($_GET["nombreA"],$_GET["nombreB"]);
+            $operateur = "/";
             break;
         case "multiplication":
-            echo multiplication($_GET["nombreA"],$_GET["nombreB"]);
+            $resultat =  multiplication($_GET["nombreA"],$_GET["nombreB"]);
+            $operateur = "*";
             break;
 
     }
+
+
+    $historique[] = $_GET["nombreA"] . $operateur . $_GET["nombreB"] . "=" . $resultat;
+    $_SESSION["historique"] = $historique;
+
+}
+
+
+?>
+<form action="" method="get">
+    <input type="text" name="nombreA"  value="<?php echo $a; ?>"  />
+    <input type="text" name="nombreB" value="<?php echo $b; ?>" />
+    <select name="calcul" >
+        <option>addition</option>
+        <option>soustraction</option>
+        <option>division</option>
+        <option>multiplication</option>
+    </select>
+<input type="submit" value="calculer" />
+</form>
+<?php
+
+  if(isset($_GET["calcul"]))  echo"Le résultat du calcul ( {$_GET["calcul"]} ) est = $resultat ";
+
+echo"<p>Historique des calculs :</p>";
+
+
+
+foreach($historique as $calcul)
+{
+    echo "<p>$calcul</p>";
 }
 
 ///étape 1 : code à fournir pour afficher le bon résultat
--- index.php
+++ index.php
@@ -2,3 +2,9 @@
 <a href="index.php?type=contact">contact</a><br />
 <a href="index.php?type=produits&autreparametres=true">produits</a><br />
 
+<?php
+session_start();
+
+$_SESSION["nom"] = "valeur";
+
+var_dump($_SESSION);
